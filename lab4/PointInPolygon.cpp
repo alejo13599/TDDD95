3d16029fd8dc7ad08b6fe3162f84b33d01c58abe
Alexander Josefsson <alejo135@student.liu.se>
2023-05-05 20:13:43 +0200

    Finished lab 4.3

--- lab4/PointInPolygon.cpp
+++ lab4/PointInPolygon.cpp
@@ -41,7 +41,7 @@ public:
       return point1.x*point2.x + point1.y*point2.y;
     }
   double point_distance(point &point1, point &point2){
-      return sqrt((point1.x-point2.x)*(point1.x-point2.x)+(point1.y-point2.y)*(point1.y-point2.y));
+      return (point1.x-point2.x)*(point1.x-point2.x)+(point1.y-point2.y)*(point1.y-point2.y);
   }
   double crossproduct(point &point1, point &point2){
       return point1.x*point2.y - point1.y*point2.x;
@@ -72,6 +72,14 @@ int checkdirection(Pointclass::point a, Pointclass::point b, Pointclass::point c
     return direction;
 }
 bool checkonline(Pointclass::point a, Pointclass::point b, Pointclass::point c){
+  Pointclass inst;
+  //cout << inst.point_distance(a,c) << " " << inst.point_distance(c,b) << " " << inst.point_distance(a,b) << "\n";
+  if(inst.point_distance(a,c)+inst.point_distance(c,b) == inst.point_distance(a,b)){
+    return true;
+  }
+  else{
+    return false;
+  }
 
 }
 
@@ -88,6 +96,7 @@ Pointclass::point testpoint = {0,0};
 cin >> NumberOfVertices;
 vector<Pointclass::point> Pointvec;
 Pointclass inst;
+bool online = false;
 while(NumberOfVertices){
     double area = 0;
     Pointvec = {};
@@ -98,26 +107,40 @@ while(NumberOfVertices){
 
     cin >> NumberOfPoints;
     for(int j=0; j<NumberOfPoints; j++){
+        online = false;
         int intersections = 0;
         cin >> testpoint.x >> testpoint.y;        
-        for(int i=0; i <NumberOfVertices; i++){
-            //cout << NumberOfPoints << " " << NumberOfVertices << "hej";
+        for(int i=0; i <NumberOfVertices-1; i++){
+            //cout << "line " << i << endl;
             int direction1 = checkdirection(Pointvec[i], Pointvec[i+1], testpoint);
-            int direction2 = checkdirection(Pointvec[i], Pointvec[i+1], {10001, 10001});
-            int direction3 = checkdirection(testpoint, {10001, 10001}, Pointvec[i]);
-            int direction4 = checkdirection(testpoint, {10001, 10001}, Pointvec[i+1]);
+            int direction2 = checkdirection(Pointvec[i], Pointvec[i+1], {40000, testpoint.y});
+            int direction3 = checkdirection(testpoint, {40000, testpoint.y}, Pointvec[i]);
+            int direction4 = checkdirection(testpoint, {40000, testpoint.y}, Pointvec[i+1]);
+            if(checkonline(Pointvec[i], Pointvec[i+1], testpoint)){
+              online = true;
+
+            }
             if(direction1 != direction2 && direction3 != direction4){
                 intersections += 1;
+                //cout << "intersection " << i+1 << "\n" ;
             }
 
 
         }
         int direction1 = checkdirection(Pointvec[NumberOfVertices-1], Pointvec[0], testpoint);
-        int direction2 = checkdirection(Pointvec[NumberOfVertices-1], Pointvec[0], {10001, 10001});
-        int direction3 = checkdirection(testpoint, {10001, 10001}, Pointvec[NumberOfVertices-1]);
-        int direction4 = checkdirection(testpoint, {10001, 10001}, Pointvec[0]);
-        if(direction1 != direction2 && direction3 != direction4){
+        int direction2 = checkdirection(Pointvec[NumberOfVertices-1], Pointvec[0], {40000, testpoint.y});
+        int direction3 = checkdirection(testpoint, {40000, testpoint.y}, Pointvec[NumberOfVertices-1]);
+        int direction4 = checkdirection(testpoint, {40000, testpoint.y}, Pointvec[0]);
+        if(checkonline(Pointvec[NumberOfVertices-1], Pointvec[0], testpoint)){
+          online = true;
+        }
+        if(online){
+          cout << "on\n";
+          continue;  
+        }
+        if(direction1 != direction2 && direction3 != direction4){    
             intersections += 1;
+           //cout << "intersection " << NumberOfVertices << "\n" ;
         }
         if(intersections%2 == 1){
             cout << "in\n";
---
+++ lab4/SegmentIntersection.cpp
@@ -0,0 +1,223 @@
+// Author: Alexander Josefsson
+// Liuid: Alejo135
+// Problem is:
+// Time-complexity: 
+// Memory-complexity:
+// Note: 
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+double delta;
+bool online = false;
+class Pointclass{
+public:
+  struct point {
+    double x, y;
+
+    // constructor
+    point(double _x, double _y) : x(_x), y(_y) {}
+
+    // add and subtract vectors
+    point operator+(const point &other) const {
+      return point(x + other.x, y + other.y);
+    }
+
+    point operator-(const point &other) const {
+      return point(x - other.x, y - other.y);
+    }
+
+    point operator*(double K) const { //Scalar multiplication
+      return point(K*x, K*y);
+    }
+
+    point operator/(double K) const { //Scalar division
+      return point(x/K, y/K);
+    }
+
+  };
+  double dotproduct(point &point1, point &point2){
+      return point1.x*point2.x + point1.y*point2.y;
+    }
+  double point_distance(point &point1, point &point2){
+      return (point1.x-point2.x)*(point1.x-point2.x)+(point1.y-point2.y)*(point1.y-point2.y);
+  }
+  double crossproduct(point &point1, point &point2){
+      return point1.x*point2.y - point1.y*point2.x;
+    }
+  
+};
+double checkarea(vector<Pointclass::point> Pointvec){
+  Pointclass inst;
+  double area = 0;
+  for(int i=0; i<Pointvec.size()-1; i++){
+      area += inst.crossproduct(Pointvec[i],Pointvec[i+1]);
+  }
+  area += inst.crossproduct(Pointvec[Pointvec.size()-1],Pointvec[0]);
+  area = area/2;
+  return area;
+}
+int checkdirection(Pointclass::point a, Pointclass::point b, Pointclass::point c){
+    int direction;
+    vector<Pointclass::point> checkdirectionvector;
+    checkdirectionvector.push_back(a);
+    checkdirectionvector.push_back(b);
+    checkdirectionvector.push_back(c);
+    if(checkarea(checkdirectionvector) < 0){
+        direction = -1;
+    }else if(checkarea(checkdirectionvector) == 0) {
+        direction = 0;
+    }
+    else{
+        direction = 1;
+    }
+    return direction;
+}
+Pointclass::point findintersection(Pointclass::point a, Pointclass::point b, Pointclass::point c, Pointclass::point d){
+  double eq1 = b.y - a.y;
+  double eq2 = a.x - b.x;
+  double eq3 = eq1*(a.x) + eq2*(a.y);
+
+  double eq4 = d.y - c.y;
+  double eq5 = c.x - d.x;
+  double eq6 = eq4*(c.x) + eq5*(c.y);
+
+  double det = eq1*eq5 - eq4*eq2;
+
+  double coord_x = (eq5*eq3 - eq2*eq6)/det;
+  double coord_y = (eq1*eq6 - eq4*eq3)/det;
+  return {coord_x,coord_y};
+
+} 
+bool checkonline(Pointclass::point a, Pointclass::point b, Pointclass::point c){
+  if(b.x <= max(a.x, c.x) && b.x >= min(a.x, c.x) && b.y <= max(a.y, c.y) && b.y >= min(a.y, c.y)){
+    return true;
+  }
+  else{return false;}
+}
+double checksign(double NumberToCheck){
+  if(abs(NumberToCheck) <= delta){
+    NumberToCheck = abs(NumberToCheck);
+  }
+  cout << fixed;
+  cout << setprecision(2);
+  return NumberToCheck;
+}
+void findleftandright(Pointclass::point a, Pointclass::point b){
+  if(a.x == b.x && a.y == b.y){
+    cout << a.x << " " << a.y << endl;
+    return; 
+  }
+  if(a.x < b.x || ((a.x == b.x) && (a.y < b.y))){
+    cout << a.x << " " << a.y << " " << b.x << " " << b.y << endl;
+  }
+  else{
+    cout << b.x << " " << b.y << " " << a.x << " " << a.y << endl;         
+  }
+}
+int checkintersection(Pointclass::point a, Pointclass::point b, Pointclass::point c, Pointclass::point d){
+    vector<Pointclass::point> Pointvec = {};
+    int intersect = 0;
+    int direction1 = checkdirection(a, b, c);
+    int direction2 = checkdirection(a, b, d);
+    int direction3 = checkdirection(c, d, a);
+    int direction4 = checkdirection(c, d, b);
+
+    if(direction1 != direction2 && direction3 != direction4){
+        intersect = 1;
+        return intersect;
+    }
+    Pointclass::point minpoint={11000,11000}, maxpoint={-11000,-11000};
+    if (direction1 == 0 && checkonline(a, c, b)){
+      Pointvec.push_back({c.x,c.y});
+      online = true;
+    }
+    if (direction2 == 0 && checkonline(a, d, b)){
+      Pointvec.push_back({d.x,d.y});
+      online = true;
+    } 
+    if (direction3 == 0 && checkonline(c, a, d)){
+      Pointvec.push_back({a.x,a.y});
+      online = true;
+    }
+    if (direction4 == 0 && checkonline(c, b, d)){ 
+      Pointvec.push_back({b.x,b.y});
+      online = true;
+    }
+    if(online){
+      if(Pointvec.size() == 2){
+        findleftandright(Pointvec[0], Pointvec[1]);
+      }
+      if(Pointvec.size() == 3){
+        Pointclass inst;
+        double longestdist = inst.point_distance(Pointvec[0], Pointvec[1]);
+        minpoint={Pointvec[0]}, maxpoint={Pointvec[1]};
+        if(longestdist < inst.point_distance(Pointvec[0], Pointvec[2])){
+            longestdist = inst.point_distance(Pointvec[0], Pointvec[2]);
+            minpoint={Pointvec[0]}, maxpoint={Pointvec[2]};
+        }   
+        if(longestdist < inst.point_distance(Pointvec[1], Pointvec[2])){
+            longestdist = inst.point_distance(Pointvec[1], Pointvec[2]);
+            minpoint={Pointvec[1]}, maxpoint={Pointvec[2]};
+        }
+        findleftandright(minpoint, maxpoint);                      
+      }
+
+      if(Pointvec.size() == 4){ 
+        Pointclass inst;
+        double longestdist = inst.point_distance(Pointvec[0], Pointvec[1]);
+        minpoint={Pointvec[0]}, maxpoint={Pointvec[1]};
+        if(longestdist < inst.point_distance(Pointvec[0], Pointvec[2])){
+            longestdist = inst.point_distance(Pointvec[0], Pointvec[2]);
+            minpoint={Pointvec[0]}, maxpoint={Pointvec[2]};
+        }
+        if(longestdist < inst.point_distance(Pointvec[0], Pointvec[3])){
+            longestdist = inst.point_distance(Pointvec[0], Pointvec[3]);
+            minpoint={Pointvec[0]}, maxpoint={Pointvec[3]};
+        }   
+        if(longestdist < inst.point_distance(Pointvec[1], Pointvec[2])){
+            longestdist = inst.point_distance(Pointvec[1], Pointvec[2]);
+            minpoint={Pointvec[1]}, maxpoint={Pointvec[2]};
+        }       
+        if(longestdist < inst.point_distance(Pointvec[1], Pointvec[3])){
+            longestdist = inst.point_distance(Pointvec[1], Pointvec[3]);
+            minpoint={Pointvec[1]}, maxpoint={Pointvec[3]};
+        }
+        if(longestdist < inst.point_distance(Pointvec[2], Pointvec[3])){
+            longestdist = inst.point_distance(Pointvec[2], Pointvec[3]);
+            minpoint={Pointvec[2]}, maxpoint={Pointvec[3]};
+        }
+        findleftandright(minpoint, maxpoint);          
+      }
+      return intersect;
+    }
+    intersect = -1;
+    return intersect;
+}
+
+int main() 
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+int NumberOfCases;
+double x1, y1, x2, y2, x3, y3, x4, y4;
+cin >> NumberOfCases;
+
+for(int k=0; k<NumberOfCases; k++){
+    double area = 0;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Pointclass::point point1 = {x1,y1}, point2 = {x2,y2}, point3 = {x3,y3}, point4 = {x4,y4};
+    online = false;
+    int intersections = checkintersection(point1, point2, point3, point4);
+    if(online){continue;}
+    if(intersections == 1){
+        Pointclass::point PointOfIntersection = findintersection(point1,point2,point3,point4);
+        cout << checksign(PointOfIntersection.x) << " " << checksign(PointOfIntersection.y) << endl; 
+    }
+    else{cout << "none\n";}       
+}
+return 0;
+}
